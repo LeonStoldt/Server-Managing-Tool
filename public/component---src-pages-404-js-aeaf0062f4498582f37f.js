7bf005288a9cb08c2d284f7124747824bdb470c2
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2020-06-21 20:18:48 +0200

    add public pages to repository

---
+++ public/component---src-pages-404-js-aeaf0062f4498582f37f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),u=n("vrFN"),r=n("Wbzz");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"404 NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... Return to ",l.a.createElement(r.Link,{to:"/"},"Home"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-aeaf0062f4498582f37f.js.map
